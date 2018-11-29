90e4adde462f2c51d922430b3244b7136e9dd692
Viet-Tien <tiena2cva@gmail.com>
2018-11-29 13:54:08 +0700

    add example of less 5

---
+++ source/less_5/tutorial_5/ex1_linear_known_model.py
@@ -0,0 +1,23 @@
+import numpy as np
+import tensorflow as tf
+
+
+def define_parameters():
+    a = b = 0
+    # TODO 1: Initialize parameters of model named: 'a' and 'b'
+
+    return a, b
+
+
+def define_cost_func(X, Y, a, b, n_sample):
+    h = cost = 0
+    # TODO 2: define hypothesis 'h' and cost function cost 'cost'
+
+    return cost
+
+
+def define_optimizer(l_rate, cost_func):
+    optimizer = initializer = 0
+    # TODO 3: define optimizer and initializer
+
+    return optimizer, initializer
